fb71ad9a0405838d9a7f1cd354da11ca55e4b186
Jose Manuel Vilomar <jvilomar@gmail.com>
2013-11-26 11:59:32 -0400

    Inicial Load

---
+++ DropzoneDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DropzoneDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropzoneDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CargarArchivo(Cuenta cuenta, IEnumerable<HttpPostedFileBase> file)
+        {
+            string nombreArchivo = file.FirstOrDefault().FileName;
+            string Nombres = cuenta.Nombre;
+            string Apellidos = cuenta.Apellidos;
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ DropzoneDemo/Models/Cuenta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropzoneDemo.Models
+{
+    public class Cuenta
+    {
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+    }
+}
